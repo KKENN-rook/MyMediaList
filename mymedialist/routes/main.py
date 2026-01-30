a5731bc07e519035ebda62d0210312dc19ae2425
Kevin Ngo <kevin.kngo3@gmail.com>
2026-01-29 19:21:34 -0800

    Add basic stats to profile

--- mymedialist/routes/main.py
+++ mymedialist/routes/main.py
@@ -1,5 +1,14 @@
 from flask import render_template, Blueprint
-from flask_login import login_required
+from flask_login import login_required, current_user
+
+from ..extensions import db
+from ..services.stats import get_category_stats
+ 
+CATEGORY_TITLES = {
+    "books": "Books",
+    "games": "Games",
+    "shows": "Shows & Film",
+}
 
 
 bp = Blueprint("main", __name__)
@@ -13,4 +22,8 @@ def home():
 @bp.route("/profile")
 @login_required
 def profile():
-    return render_template("profile.html")
+    stats_by_category = {}
+
+    for category in CATEGORY_TITLES.keys(): 
+        stats_by_category[category] = get_category_stats(db.session, user_id=current_user.id, category=category)
+    return render_template("profile.html", stats_by_category=stats_by_category, category_titles=CATEGORY_TITLES)
